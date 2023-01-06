a10c91e14b28c361efdc1b49032930b6beebd340
오진영 <99639919+OJOJIN@users.noreply.github.com>
2023-01-06 17:17:53 +0900

    [Gold II] Title: 트리의 지름, Time: 536 ms, Memory: 72984 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,39 @@
+from sys import stdin
+
+
+def dfs(num, w):
+    max = 0
+    check[num] = True
+
+    for node, weight in tree[num].items():
+        if not check[node]:
+            max = dfs(node, weight)
+            if max > max_child[num]:
+                max_length[num] = max_child[num] + max
+                max_child[num] = max
+            elif max + max_child[num] > max_length[num]:
+                max_length[num] = max + max_child[num]
+
+    return w + max_child[num]
+
+
+v = int(stdin.readline())
+
+tree = [dict() for i in range(v + 1)]
+max_child = [0] * (v + 1)
+max_length = [0] * (v + 1)
+check = [False] * (v + 1)
+ans = 0
+
+for i in range(1, v + 1):
+    input_list = list(map(int, stdin.readline().split()))
+    for j in range(1, len(input_list) - 1, 2):
+        tree[input_list[0]][input_list[j]] = input_list[j + 1]
+
+dfs(1, 0)
+
+for i in range(1, v + 1):
+    if max_length[i] > ans:
+        ans = max_length[i]
+
+print(ans)
