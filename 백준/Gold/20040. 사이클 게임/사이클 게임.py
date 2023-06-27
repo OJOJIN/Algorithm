c10ff8c782f1cc8095c67e98cdaba55032d5f1b3
오진영 <ojy101039@gmail.com>
2023-06-27 11:31:28 +0900

    [Gold IV] Title: 사이클 게임, Time: 924 ms, Memory: 214392 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,41 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+
+p = [i for i in range(n)]
+
+
+def find(a):
+    if p[a] == a:
+        return a
+    else:
+        return find(p[a])
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        p[b] = a
+    else:
+        p[a] = b
+
+
+a_list = []
+b_list = []
+
+ans = 0
+
+for _ in range(m):
+    a, b = map(int, stdin.readline().split())
+    a_list.append(a)
+    b_list.append(b)
+
+for i in range(m):
+    if find(a_list[i]) == find(b_list[i]):
+        ans = i + 1
+        break
+    else:
+        union(a_list[i], b_list[i])
+
+print(ans)
